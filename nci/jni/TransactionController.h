aca166ab6123abaca6df5d5250ba6566f104b095
nxf35421 <sangeetha.agarwal@nxp.com>
2018-02-06 16:00:50 +0530

    Copyright on new commits should be extended for new commits

--- nci/jni/TransactionController.h
+++ nci/jni/TransactionController.h
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- *  Copyright (C) 2015 NXP Semiconductors
+ *  Copyright (C) 2015-2018 NXP Semiconductors
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
