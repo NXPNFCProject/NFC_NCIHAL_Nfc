1a337db0b83977323a1d3869f0382f4b10467231
Suraj Uday Kotharkar <suraj.uday.kotharkar@nxp.com>
2018-05-15 15:29:47 +0530

    Align NFC code base with latest AOSP master branch
        Fix Mifare Classic Transaction Fail issue
    
        Send error response to application if transaction failed.
    
        Test: test Mifare Classic card with TagInfo

--- nci/jni/NativeNfcTag.cpp
+++ nci/jni/NativeNfcTag.cpp
@@ -973,10 +973,9 @@ static jbyteArray nativeNfcTag_doTransceive(JNIEnv* e, jobject o,
         if (EXTNS_CheckMfcResponse(&transData, &transDataLen) ==
             NFCSTATUS_FAILED) {
           nativeNfcTag_doReconnect(e, o);
-        } else {
-          if (transDataLen != 0) {
-            result.reset(e->NewByteArray(transDataLen));
-          }
+        }
+        if (transDataLen != 0) {
+          result.reset(e->NewByteArray(transDataLen));
           if (result.get() != NULL) {
             e->SetByteArrayRegion(result.get(), 0, transDataLen,
                                   (const jbyte*)transData);
