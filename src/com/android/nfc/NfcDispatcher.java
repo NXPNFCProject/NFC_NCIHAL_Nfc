320044b78d8afb4bff33c0ad54cbf940ca719712
Sachin Dhivare <sachin.dhivare@nxp.com>
2018-08-10 11:46:29 +0530

    Cannot read NDEF tag without http://
    http:// is a proper scheme for parsing ndef data for any web content defined
    by google. www is treated as just text string. Added to fix to avoid nullpointer
    dereference in case of string started with www

--- src/com/android/nfc/NfcDispatcher.java
+++ src/com/android/nfc/NfcDispatcher.java
@@ -222,6 +222,8 @@ class NfcDispatcher {
         }
 
         public boolean isWebIntent() {
+            if(ndefUri.normalizeScheme().getScheme() == null)
+                return false;
             return ndefUri != null && ndefUri.normalizeScheme().getScheme().startsWith("http");
         }
 
