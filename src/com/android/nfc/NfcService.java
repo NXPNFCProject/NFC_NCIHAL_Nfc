7b0bf0d735cbba6824838e313582a1fc3a86929a
Sachin Dhivare <sachin.dhivare@nxp.com>
2018-09-20 11:38:53 +0530

    GSMA[v13]TC_15.9.3.1.2 - IllegalArgumentException: Invalid SEName
    Added fix to check whether the applications registering to receive event
    transaction has NFC Permissions granted.

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -1139,7 +1139,11 @@ public class NfcService implements DeviceHostListener {
         synchronized (this) {
             mNfcEventInstalledPackages.clear();
             for (int i = 0; i < packagesNfcEvents.size(); i++) {
-                mNfcEventInstalledPackages.add(packagesNfcEvents.get(i).packageName);
+                int hasPerm = pm.checkPermission(android.Manifest.permission.NFC,
+                                                 packagesNfcEvents.get(i).packageName);
+                if (hasPerm == PackageManager.PERMISSION_GRANTED){
+                      mNfcEventInstalledPackages.add(packagesNfcEvents.get(i).packageName);
+                }
             }
         }
     }
