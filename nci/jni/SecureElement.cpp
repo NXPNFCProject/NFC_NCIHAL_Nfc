02ad4d2edd1d4f279f31a745635fd2d0c71d7076
Suhas Suresh <suhas.suresh_1@nxp.com>
2018-10-31 19:15:14 +0530

    Updated DWP and SPI behavior in case of RF activation
    As per the requiredment, DWP activation + transaction should be blocked
    if RF session is active till RF session deactivation.
    For SPI, activation + transaction will be allowed after Field On/Off guard
    time after activation.
    This different requirements for DWP and SPI and handled by updating source
    of the activation.

--- nci/jni/SecureElement.cpp
+++ nci/jni/SecureElement.cpp
@@ -4929,7 +4929,11 @@ tNFA_STATUS SecureElement::setNfccPwrConfig(uint8_t value) {
     }
     cur_value = value;
     SyncEventGuard guard(mPwrLinkCtrlEvent);
-    nfaStat = NFA_SendPowerLinkCommand((uint8_t)EE_HANDLE_0xF3, value);
+    bool isSpiOnReq = false;
+    if ((value == 0x03) && dual_mode_current_state & SPI_ON) {
+      isSpiOnReq = true;
+    }
+    nfaStat = NFA_SendPowerLinkCommand((uint8_t)EE_HANDLE_0xF3, value, isSpiOnReq);
     if (nfaStat == NFA_STATUS_OK && !android::nfcManager_isNfcDisabling())
       if (mPwrLinkCtrlEvent.wait(DWP_LINK_ACTV_TIMEOUT) == false) {
         DLOG_IF(ERROR, nfc_debug_enabled)
