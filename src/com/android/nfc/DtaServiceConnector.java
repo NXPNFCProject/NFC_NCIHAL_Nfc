76b0973bdc857d1267d25216a6ef0bb8633e948f
nxf35421 <sangeetha.agarwal@nxp.com>
2019-05-13 16:01:38 +0530

    Copyright year update

--- src/com/android/nfc/DtaServiceConnector.java
+++ src/com/android/nfc/DtaServiceConnector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 NXP Semiconductors
+ * Copyright (C) 2017-2019 NXP Semiconductors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/com/android/nfc/snep/SnepMessenger.java
+++ src/com/android/nfc/snep/SnepMessenger.java
@@ -30,7 +30,7 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 *
-*  Copyright 2018 NXP
+*  Copyright 2018-2019 NXP
 *
 ******************************************************************************/
 package com.android.nfc.snep;
