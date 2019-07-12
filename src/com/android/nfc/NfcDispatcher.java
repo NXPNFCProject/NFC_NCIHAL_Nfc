450eeb659323d81108fa9f24f8be16b5ed03d2ca
Ganesh Deva <ganesh.deva_1@nxp.com>
2019-07-12 11:44:56 +0530

    {Q_AOSP}: Confirmation dialog for opening NFC-scanned urls
    This adds an confirmation dialog displayed before opening an url from a
    scanned NFC tag. The feature is off by default for now and will be
    selectively enabled via a build overlay, but we plan to turn this on
    unconditionally in the future.

--- src/com/android/nfc/NfcDispatcher.java
+++ src/com/android/nfc/NfcDispatcher.java
@@ -16,6 +16,7 @@
 
 package com.android.nfc;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.bluetooth.BluetoothAdapter;
 import android.os.UserManager;
@@ -26,6 +27,7 @@ import com.android.nfc.handover.PeripheralHandoverService;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.app.IActivityManager;
 import android.app.PendingIntent;
 import android.app.PendingIntent.CanceledException;
@@ -33,6 +35,7 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
@@ -52,6 +55,10 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -61,6 +68,7 @@ import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Dispatch of NFC events to start activities
@@ -199,6 +207,14 @@ class NfcDispatcher {
             intent.setType(null);
             intent.setAction(NfcAdapter.ACTION_TAG_DISCOVERED);
             return intent;
+        }
+
+        public boolean isWebIntent() {
+            return ndefUri != null && ndefUri.normalizeScheme().getScheme().startsWith("http");
+        }
+
+        public String getUri() {
+            return ndefUri.toString();
         }
 
         /**
@@ -524,6 +540,13 @@ class NfcDispatcher {
 
         // regular launch
         dispatch.intent.setPackage(null);
+
+        if (dispatch.isWebIntent()) {
+            if (DBG) Log.i(TAG, "matched Web link - prompting user");
+            showWebLinkConfirmation(dispatch);
+            return true;
+        }
+
         if (dispatch.tryStartActivity()) {
             if (DBG) Log.i(TAG, "matched NDEF");
             return true;
@@ -694,6 +717,33 @@ class NfcDispatcher {
             Log.d(TAG, "Component not enabled: " + compname);
         }
         return enabled;
+    }
+
+    void showWebLinkConfirmation(DispatchInfo dispatch) {
+        if (!mContext.getResources().getBoolean(R.bool.enable_nfc_url_open_dialog)) {
+            dispatch.tryStartActivity();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                mContext.getApplicationContext(),
+                android.R.style.Theme_DeviceDefault_Light_Dialog_Alert);
+        builder.setTitle(R.string.title_confirm_url_open);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.url_open_confirmation, null);
+        if (view != null) {
+            TextView url = view.findViewById(R.id.url_open_confirmation_link);
+            if (url != null) {
+                url.setText(dispatch.getUri());
+            }
+            builder.setView(view);
+        }
+        builder.setNegativeButton(R.string.cancel, (dialog, which) -> {});
+        builder.setPositiveButton(R.string.action_confirm_url_open, (dialog, which) -> {
+            dispatch.tryStartActivity();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        dialog.show();
     }
 
     void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
