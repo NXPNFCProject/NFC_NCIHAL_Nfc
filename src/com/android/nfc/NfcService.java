05c4ebe58bc5e6ccef8c6a399c574a6265cc767c
siddaram khed <siddaram.khed@nxp.com>
2019-08-30 16:57:19 +0530

    {QSSI} Fix duplicate routing update during dual UICC switch
    During dualUicc switch if there are AIDs, then commitRouting is called after
    AIDs are sent.Then, as part of dualUICC switch, again commitRouting is called
    (required in case if no AIDs available). This causes duplicate routing table update.
    To avoid this, an existing flag is used, which will be set if commitRouting
    is called as part of AID routing. If this flag is set,the do not call
    commit routing again.

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -1985,9 +1985,11 @@ public class NfcService implements DeviceHostListener {
                         mDeviceHost.clearRoutingEntry(AID_ENTRY);
                         mDeviceHost.clearRoutingEntry(TECH_ENTRY);
                         mDeviceHost.clearRoutingEntry(PROTOCOL_ENTRY);
-                        mCardEmulationManager.onNfcEnabled();
                         computeRoutingParameters();
-                        commitRouting();
+                        mCardEmulationManager.onNfcEnabled();
+                        if (getLastCommitRoutingStatus() == false) {
+                          commitRouting();
+                        }
                     }
                     else
                     {
