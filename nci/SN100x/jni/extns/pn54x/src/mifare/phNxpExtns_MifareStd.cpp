4e8ed2e88bd078382e89640fcbce7c377e4b5b86
Lijo Abraham <lijon.abraham@nxp.com>
2019-09-06 10:55:14 +0530

    Prevent OOB write in phFriNfc_ExtnsTransceive

--- nci/SN100x/jni/extns/pn54x/src/mifare/phNxpExtns_MifareStd.cpp
+++ nci/SN100x/jni/extns/pn54x/src/mifare/phNxpExtns_MifareStd.cpp
@@ -35,6 +35,7 @@
 
 #include <android-base/stringprintf.h>
 #include <base/logging.h>
+#include <log/log.h>
 #include <nfc_api.h>
 #include <nfc_int.h>
 #include <phNfcCompId.h>
@@ -1873,6 +1874,11 @@ NFCSTATUS phFriNfc_ExtnsTransceive(phNfc_sTransceiveInfo_t* pTransceiveInfo,
   uint8_t restore_payload[] = {
       0x00, 0x00, 0x00, 0x00,
   };
+
+  if (SendLength == 0) {
+    android_errorWriteLog(0x534e4554, "132083376");
+    return status;
+  }
 
   buff = (uint8_t*)malloc((uint32_t)MAX_BUFF_SIZE);
   if (NULL == buff) {
