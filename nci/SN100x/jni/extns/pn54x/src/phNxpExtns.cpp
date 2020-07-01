7bc75ee9548e6025792a55841eafdc5e2090f97a
nxf63550 <bhautik.ardeshana@nxp.com>
2020-07-01 15:37:19 +0530

    Remove NxpLog NxpConfig
    loglevel is controlled by nfc_debug_enabled
    Don't need to read config and set loglevel anymore

--- nci/SN100x/jni/extns/pn54x/src/phNxpExtns.cpp
+++ nci/SN100x/jni/extns/pn54x/src/phNxpExtns.cpp
@@ -43,7 +43,6 @@
 #include <base/logging.h>
 #include "nfc_config.h"
 #include <phNxpExtns_MifareStd.h>
-#include <phNxpLog.h>
 
 using android::base::StringPrintf;
 
@@ -77,9 +76,6 @@ static int fd_ese_nfc_sync; /*file descriptor to hold sync driver handle*/
 NFCSTATUS EXTNS_Init(tNFA_DM_CBACK* p_nfa_dm_cback,
                      tNFA_CONN_CBACK* p_nfa_conn_cback) {
   NFCSTATUS status = NFCSTATUS_FAILED;
-
-  /* Initialize Log level */
-  phNxpLog_InitializeLogLevel();
 
   /* Validate parameters */
   if ((!p_nfa_dm_cback) || (!p_nfa_conn_cback)) {
