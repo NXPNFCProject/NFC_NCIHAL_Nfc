65e63430e3da81203247580178f038977c82fd5f
Ganesh Deva <ganesh.deva_1@nxp.com>
2020-08-11 12:00:57 +0530

    Fix UserdId value when beam is not supported

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -4441,6 +4441,7 @@ public class NfcService implements DeviceHostListener {
                 sendMessage(NfcService.MSG_APPLY_SCREEN_STATE, screenState);
             } else if (action.equals(Intent.ACTION_USER_SWITCHED)) {
                 int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, 0);
+                mUserId = userId;
                 if (mIsBeamCapable) {
                     int beamSetting =
                         PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
@@ -4455,7 +4456,6 @@ public class NfcService implements DeviceHostListener {
                         Log.e(TAG, "Error int getComponentEnabledSetting for BeamShareActivity");
                     }
                     synchronized (this) {
-                        mUserId = userId;
                         if (beamSetting == PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
                            mIsNdefPushEnabled = false;
                         } else {
