4dfc0e23f91d813cf1a72e1106134d470cb4d8e2
nxf63550 <bhautik.ardeshana@nxp.com>
2020-08-14 17:23:25 +0530

    Prevent OOB write in phNciNfc_RecvMfResp

--- nci/SN100x/jni/extns/pn54x/src/mifare/phNxpExtns_MifareStd.cpp
+++ nci/SN100x/jni/extns/pn54x/src/mifare/phNxpExtns_MifareStd.cpp
@@ -1200,7 +1200,11 @@ static NFCSTATUS phNciNfc_RecvMfResp(phNciNfc_Buff_t* RspBuffInfo,
             }
             gAuthCmdBuf.auth_status = true;
             status = NFCSTATUS_SUCCESS;
-
+            if ((PHNCINFC_EXTNID_SIZE + PHNCINFC_EXTNSTATUS_SIZE) >
+                RspBuffInfo->wLen) {
+              android_errorWriteLog(0x534e4554, "126204073");
+              return NFCSTATUS_FAILED;
+            }
             /* DataLen = TotalRecvdLen - (sizeof(RspId) + sizeof(Status)) */
             wPldDataSize = ((RspBuffInfo->wLen) -
                             (PHNCINFC_EXTNID_SIZE + PHNCINFC_EXTNSTATUS_SIZE));
