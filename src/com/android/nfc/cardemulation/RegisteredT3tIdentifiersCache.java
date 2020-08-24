dd19375d373a0a385133ce1a71c4266e8f3c4ac4
Ganesh Deva <ganesh.deva_1@nxp.com>
2020-08-24 15:18:39 +0530

    Align NFCservice code as per tag android-r beta3
    (cherry picked from commit d18f03b276c13a40c316c97f82df6563b37914ad)

--- src/com/android/nfc/cardemulation/RegisteredT3tIdentifiersCache.java
+++ src/com/android/nfc/cardemulation/RegisteredT3tIdentifiersCache.java
@@ -13,6 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ /******************************************************************************
+*
+*  The original Work has been changed by NXP.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Copyright 2019 NXP
+*
+******************************************************************************/
 
 package com.android.nfc.cardemulation;
 
@@ -133,6 +152,7 @@ public class RegisteredT3tIdentifiersCache {
             Log.d(TAG, "Not updating routing table because NFC is off.");
             return;
         }
+
         List<T3tIdentifier> t3tIdentifiers = new ArrayList<T3tIdentifier>();
 
         // Sending an empty table will de-register all entries
