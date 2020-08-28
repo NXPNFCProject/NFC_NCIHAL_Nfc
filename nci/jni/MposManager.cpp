5caa210c1258453d69d7768753e4af77edabe162
nxf36763 <ashish.manohar@nxp.com>
2020-08-28 19:48:43 +0530

    Copyright banner update for nxp_nci_hal files
    The Copyright year is updated.

--- nci/jni/MposManager.cpp
+++ nci/jni/MposManager.cpp
@@ -12,7 +12,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Copyright 2018-2019 NXP
+ *  Copyright 2018-2020 NXP
  *
  ******************************************************************************/
 #include <android-base/stringprintf.h>
