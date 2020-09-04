d311b310e3e7148184ae248e808061e425f17c8d
nxf56319 <sai.shwethas@nxp.com>
2020-09-04 15:52:10 +0530

    Fix for Coverity
    Recovered UNUSED_VALUE error:
    Changed the code so that descriptionString value is
    not overwritten before it is used.

--- src/com/android/nfc/cardemulation/RegisteredNfcFServicesCache.java
+++ src/com/android/nfc/cardemulation/RegisteredNfcFServicesCache.java
@@ -440,24 +440,18 @@ public class RegisteredNfcFServicesCache {
                                     parser.getAttributeValue(null, "component");
                             String uidString =
                                     parser.getAttributeValue(null, "uid");
-                            String systemCodeString =
-                                    parser.getAttributeValue(null, "system-code");
-                            String descriptionString =
-                                    parser.getAttributeValue(null, "description");
-                            String nfcid2String =
-                                    parser.getAttributeValue(null, "nfcid2");
                             if (compString == null || uidString == null) {
                                 Log.e(TAG, "Invalid service attributes");
                             } else {
                                 try {
                                     componentName = ComponentName.unflattenFromString(compString);
                                     currentUid = Integer.parseInt(uidString);
-                                    systemCode = systemCodeString;
-                                    description = descriptionString;
-                                    nfcid2 = nfcid2String;
                                 } catch (NumberFormatException e) {
                                     Log.e(TAG, "Could not parse service uid");
                                 }
+                                systemCode = parser.getAttributeValue(null, "system-code");
+                                description = parser.getAttributeValue(null, "description");
+                                nfcid2 = parser.getAttributeValue(null, "nfcid2");
                             }
                         }
                     } else if (eventType == XmlPullParser.END_TAG) {
