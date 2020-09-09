8c0cbeda6ec5f690b03285dd162e5e2cf95d5050
Hariprasad Nalacheruvu <nxp79939@lsv03234.swis.in-blr01.nxp.com>
2020-09-09 16:54:47 +0530

    Keep screen on only when dispatch succeed
    Bug: 159865156
    Bug: 65353992
    Test: Manual (Read NFC tag and check screen state)
    Change-Id: Ib3d79cd6cc88bafb729808a2cfc8d8a24a726f44

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -3457,10 +3457,6 @@ public class NfcService implements DeviceHostListener {
 
                 case MSG_NDEF_TAG:
                     if (DBG) Log.d(TAG, "Tag detected, notifying applications");
-                    if (mScreenState == ScreenStateHelper.SCREEN_STATE_ON_UNLOCKED) {
-                        mPowerManager.userActivity(SystemClock.uptimeMillis(),
-                                PowerManager.USER_ACTIVITY_EVENT_OTHER, 0);
-                    }
                     mNumTagsDetected.incrementAndGet();
                     TagEndpoint tag = (TagEndpoint) msg.obj;
                     byte[] debounceTagUid;
@@ -4212,6 +4208,10 @@ public class NfcService implements DeviceHostListener {
                             playSound(SOUND_END);
                         }
                         if (readerParams.callback != null) {
+                            if (mScreenState == ScreenStateHelper.SCREEN_STATE_ON_UNLOCKED) {
+                                mPowerManager.userActivity(SystemClock.uptimeMillis(),
+                                        PowerManager.USER_ACTIVITY_EVENT_OTHER, 0);
+                            }
                             readerParams.callback.onTagDiscovered(tag);
                             return;
                         } else {
@@ -4257,6 +4257,10 @@ public class NfcService implements DeviceHostListener {
                         if (DBG) Log.d(TAG, "Tag dispatch failed notification");
                     }
                 } else if (dispatchResult == NfcDispatcher.DISPATCH_SUCCESS) {
+                    if (mScreenState == ScreenStateHelper.SCREEN_STATE_ON_UNLOCKED) {
+                        mPowerManager.userActivity(SystemClock.uptimeMillis(),
+                                PowerManager.USER_ACTIVITY_EVENT_OTHER, 0);
+                    }
                     mDispatchFailedCount = 0;
                     mVibrator.vibrate(mVibrationEffect);
                     playSound(SOUND_END);
