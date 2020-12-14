1fba160dc215fb958ac12c2ddb2598b0c9f35163
nxf36763 <ashish.manohar@nxp.com>
2020-12-14 23:42:38 +0530

    Make read error toast configurable
    Sometimes nfc read error toast would reset screen timeout,
    because NFC would detect the same tag every few seconds
    when unknown_tag_polling_delay enabled.
    
    Set read error toast default disable,
    since read error toast conflicts with unknown_tag_polling_delay.
    
    Test: manual
    Bug: 159865156
    Merged-In: I0ce6e53ba2dcb8a7780437ec8e9d21fb303e09ce
    Change-Id: I0ce6e53ba2dcb8a7780437ec8e9d21fb303e09ce

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -485,6 +485,7 @@ public class NfcService implements DeviceHostListener {
 
     int mPollDelay;
     boolean mNotifyDispatchFailed;
+    boolean mNotifyReadFailed;
 
     private NfcDispatcher mNfcDispatcher;
     private PowerManager mPowerManager;
@@ -858,7 +859,9 @@ public class NfcService implements DeviceHostListener {
 
         // Polling delay variables
         mPollDelay = mContext.getResources().getInteger(R.integer.unknown_tag_polling_delay);
-        mNotifyDispatchFailed = mContext.getResources().getBoolean(R.bool.enable_notify_dispatch_failed);
+        mNotifyDispatchFailed =
+            mContext.getResources().getBoolean(R.bool.enable_notify_dispatch_failed);
+        mNotifyReadFailed = mContext.getResources().getBoolean(R.bool.enable_notify_read_failed);
 
         mPollingDisableAllowed = mContext.getResources().getBoolean(R.bool.polling_disable_allowed);
 
@@ -3658,7 +3661,7 @@ public class NfcService implements DeviceHostListener {
                         if (!tag.reconnect()) {
                             tag.disconnect();
                             if (mScreenState == ScreenStateHelper.SCREEN_STATE_ON_UNLOCKED) {
-                                if (!sToast_debounce) {
+                                if (!sToast_debounce && mNotifyReadFailed) {
                                     Toast.makeText(mContext, R.string.tag_read_error,
                                                    Toast.LENGTH_SHORT).show();
                                     sToast_debounce = true;
