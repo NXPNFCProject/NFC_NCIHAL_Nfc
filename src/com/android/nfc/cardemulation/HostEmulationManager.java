4972d3bb59190471225bf8fe8bfffa8070d971e4
Ravindra Yashvant Shinde <ravindra.yashvant.shinde@nxp.com>
2021-02-02 09:16:49 +0530

    Fix bug with NFC APDU service rebinding

--- src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -29,7 +29,7 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 *
-*  Copyright 2018-2020 NXP
+*  Copyright 2018-2021 NXP
 *
 ******************************************************************************/
 package com.android.nfc.cardemulation;
@@ -498,6 +498,7 @@ public class HostEmulationManager {
             synchronized (mLock) {
                 Log.d(TAG, "Service unbound");
                 mService = null;
+                mServiceName = null;
                 mServiceBound = false;
             }
         }
