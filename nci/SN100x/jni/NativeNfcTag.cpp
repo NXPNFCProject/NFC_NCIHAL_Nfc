1d4852ad93bf15cd33bd2e39041819a645fed74b
Ayush Raj <ayush.raj@nxp.com>
2021-02-23 16:27:13 +0530

    Give priority to card mode over reader mode
    To avoid semantic error and synchronize between NFCC & DH.

--- nci/SN100x/jni/NativeNfcTag.cpp
+++ nci/SN100x/jni/NativeNfcTag.cpp
@@ -61,6 +61,7 @@
 #if(NXP_SRD == TRUE)
 #include "SecureDigitization.h"
 #endif
+#include "SecureElement.h"
 #endif
 
 using android::base::StringPrintf;
@@ -823,6 +824,13 @@ static int reSelect(tNFA_INTF_TYPE rfInterface, bool fSwitchIfNeeded) {
           break;
         }
       } else {
+        if (SecureElement::getInstance().isRfFieldOn()) {
+          rVal = STATUS_CODE_TARGET_LOST;
+          NfcTag::getInstance().resetActivationState();
+          DLOG_IF(INFO, nfc_debug_enabled)
+              << StringPrintf("%s: card emulation on priotiy", __func__);
+          break;
+        }
 #endif
         DLOG_IF(INFO, nfc_debug_enabled)
             << StringPrintf("%s: deactivate to sleep", __func__);
