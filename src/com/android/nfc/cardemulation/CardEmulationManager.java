79fcd8d2a82b8db70ea4039f9eb3bef6ed70cedb
nxf32288 <ganesh.deva_1@nxp.com>
2022-02-11 18:59:41 +0530

    Fix incorrect checking logic for preferred service
    Personal and managed profiles are now supported.
    Checking ComponentName for mPreferredPaymentService and
    mPreferredForegroundService is not enough.
    Add userId of the ApduServiceInfo comparision into the checking flow.

--- src/com/android/nfc/cardemulation/CardEmulationManager.java
+++ src/com/android/nfc/cardemulation/CardEmulationManager.java
@@ -285,7 +285,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
     void verifyDefaults(int userId, List<ApduServiceInfo> services) {
         ComponentName defaultPaymentService =
                 getDefaultServiceForCategory(userId, CardEmulation.CATEGORY_PAYMENT, true);
-        if (DBG) Log.d(TAG, "Current default: " + defaultPaymentService);
+        if (DBG) Log.d(TAG, "Current default: " + defaultPaymentService + " for user:" + userId);
         if (defaultPaymentService == null) {
             // A payment service may have been removed, set default payment selection to "not set".
             if (DBG) Log.d(TAG, "No default set, last payment service removed.");
@@ -309,7 +309,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
                 return service;
             } else {
                 return mServiceCache.hasService(userId, service) ? service : null;
-             }
+            }
         } else {
             return null;
         }
@@ -653,7 +653,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
 
     @Override
     public void onPreferredPaymentServiceChanged(int userId, ComponentName service) {
-        mAidCache.onPreferredPaymentServiceChanged(service);
+        mAidCache.onPreferredPaymentServiceChanged(userId, service);
         mHostEmulationManager.onPreferredPaymentServiceChanged(userId, service);
 
         NfcService.getInstance().onPreferredPaymentChanged(
@@ -662,7 +662,7 @@ public class CardEmulationManager implements RegisteredServicesCache.Callback,
 
     @Override
     public void onPreferredForegroundServiceChanged(int userId, ComponentName service) {
-        mAidCache.onPreferredForegroundServiceChanged(service);
+        mAidCache.onPreferredForegroundServiceChanged(userId, service);
         mHostEmulationManager.onPreferredForegroundServiceChanged(userId, service);
 
         NfcService.getInstance().onPreferredPaymentChanged(
