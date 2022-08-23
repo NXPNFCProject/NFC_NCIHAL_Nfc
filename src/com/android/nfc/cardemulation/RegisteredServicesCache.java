dae24df5327ab9f0aede061fc5e96e5d53abd390
nxf32288 <ganesh.deva_1@nxp.com>
2022-08-23 23:38:42 +0530

    Fix compilation issue.

--- src/com/android/nfc/cardemulation/RegisteredServicesCache.java
+++ src/com/android/nfc/cardemulation/RegisteredServicesCache.java
@@ -232,14 +232,14 @@ public class RegisteredServicesCache {
     public void onUserSwitched() {
         synchronized (mLock) {
             refreshUserProfilesLocked();
-            invalidateCache(ActivityManager.getCurrentUser());
+            invalidateCache(ActivityManager.getCurrentUser(), true);
         }
     }
 
     public void onManagedProfileChanged() {
         synchronized (mLock) {
             refreshUserProfilesLocked();
-            invalidateCache(ActivityManager.getCurrentUser());
+            invalidateCache(ActivityManager.getCurrentUser(), true);
         }
     }
 
