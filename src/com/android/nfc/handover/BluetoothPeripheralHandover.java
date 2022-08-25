8b5777c753750e753aec65ce0010901283c53b98
ashish_manohar <ashish.manohar@nxp.com>
2022-08-25 16:08:57 +0530

    API Review: add `UNBOND` in `EXTRA_REASON` name
    > Can you make EXTRA_REASON an 'unsupported app usage' alias for
    > EXTRA_UNBOUND_REASON (which is in the public API)?
    > And add a max target SDK to the @UnsupportedAppUsage annotation.
    
    Change-Id: Icf3118234e90d6938f09e9e0238396a823250e57

--- src/com/android/nfc/handover/BluetoothPeripheralHandover.java
+++ src/com/android/nfc/handover/BluetoothPeripheralHandover.java
@@ -271,7 +271,7 @@ public class BluetoothPeripheralHandover implements BluetoothProfile.ServiceList
                         if (mInput.getConnectionState(mDevice)
                                 != BluetoothProfile.STATE_DISCONNECTED) {
                             mHidResult = RESULT_PENDING;
-                            mInput.disconnect(mDevice);
+                            mDevice.disconnect();
                             toast(getToastString(R.string.disconnecting_peripheral));
                             break;
                         } else {
@@ -281,18 +281,17 @@ public class BluetoothPeripheralHandover implements BluetoothProfile.ServiceList
                         if (mHeadset.getConnectionState(mDevice)
                                 != BluetoothProfile.STATE_DISCONNECTED) {
                             mHfpResult = RESULT_PENDING;
-                            mHeadset.disconnect(mDevice);
                         } else {
                             mHfpResult = RESULT_DISCONNECTED;
                         }
                         if (mA2dp.getConnectionState(mDevice)
                                 != BluetoothProfile.STATE_DISCONNECTED) {
                             mA2dpResult = RESULT_PENDING;
-                            mA2dp.disconnect(mDevice);
                         } else {
                             mA2dpResult = RESULT_DISCONNECTED;
                         }
                         if (mA2dpResult == RESULT_PENDING || mHfpResult == RESULT_PENDING) {
+                            mDevice.disconnect();
                             toast(getToastString(R.string.disconnecting_peripheral));
                             break;
                         }
@@ -387,7 +386,8 @@ public class BluetoothPeripheralHandover implements BluetoothProfile.ServiceList
                                 BluetoothProfile.STATE_CONNECTED) {
                             if (mIsHeadsetAvailable) {
                                 mHfpResult = RESULT_PENDING;
-                                mHeadset.connect(mDevice);
+                                mHeadset.setConnectionPolicy(mDevice,
+                                    BluetoothProfile.CONNECTION_POLICY_ALLOWED);
                             } else {
                                 mHfpResult = RESULT_DISCONNECTED;
                             }
@@ -397,7 +397,8 @@ public class BluetoothPeripheralHandover implements BluetoothProfile.ServiceList
                         if (mA2dp.getConnectionState(mDevice) != BluetoothProfile.STATE_CONNECTED) {
                             if (mIsA2dpAvailable) {
                                 mA2dpResult = RESULT_PENDING;
-                                mA2dp.connect(mDevice);
+                                mA2dp.setConnectionPolicy(mDevice,
+                                    BluetoothProfile.CONNECTION_POLICY_ALLOWED);
                             } else {
                                 mA2dpResult = RESULT_DISCONNECTED;
                             }
@@ -485,7 +486,7 @@ public class BluetoothPeripheralHandover implements BluetoothProfile.ServiceList
                 mRetryCount = 0;
                 nextStepConnect();
             } else if (bond == BluetoothDevice.BOND_NONE) {
-                int reason = intent.getIntExtra(BluetoothDevice.EXTRA_REASON,
+                int reason = intent.getIntExtra(BluetoothDevice.EXTRA_UNBOND_REASON,
                         BluetoothAdapter.ERROR);
                 if (mRetryCount < MAX_RETRY_COUNT
                         && reason != BluetoothDevice.UNBOND_REASON_AUTH_FAILED) {
