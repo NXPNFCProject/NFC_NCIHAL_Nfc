13fd0449fd8f795003060d20eebd10aa2c6b1060
nxf63690 <sourav.kumar@nxp.com>
2023-03-14 11:19:13 +0530

    nfc(service): Throw illegal state exception when not in fg
    Instead of checking this in the API class, throw a remote exception from
    service when we detect that a non-fg app is trying to enable/disable fg
    dispatch.
    
    Bug: 263565738
    Test: atest CtsNfcTestCases
    Test: CtsVerifier tag tests
    
    Change-Id: Ibdee799c2c642c4170c081d1b2e44c917eb77fd1

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -1801,8 +1801,8 @@ public class NfcService implements DeviceHostListener {
                 IntentFilter[] filters, TechListParcel techListsParcel) {
             NfcPermissions.enforceUserPermissions(mContext);
             if (!mForegroundUtils.isInForeground(Binder.getCallingUid())) {
-                Log.e(TAG, "setForegroundDispatch: Caller not in foreground.");
-                return;
+                throw new IllegalStateException("Foreground dispatch can only be enabled/disabled "
+                        + "when your activity is in foreground");
             }
             // Short-cut the disable path
             if (intent == null && filters == null && techListsParcel == null) {
