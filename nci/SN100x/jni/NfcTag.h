3b8915d965b843c2c32824ca0fcdba55ffec1fc8
nxf31698 <suryaprakash.konduru@nxp.com>
2023-07-31 09:25:20 +0530

    Fix for Tag transceive timeout is set with unintended big value
    MIN_FWI is set to 11 & MAX_FWI is set to 14
    
    Change-Id: Ieee23d8a41a7ff4da8be9fb4dc0fe2dd08cd4d3b

--- nci/SN100x/jni/NfcTag.h
+++ nci/SN100x/jni/NfcTag.h
@@ -18,33 +18,34 @@
  *  Tag-reading, tag-writing operations.
  */
 /******************************************************************************
-*
-*  The original Work has been changed by NXP.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*  http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-*
-*  Copyright 2020-2022 NXP
-*
-******************************************************************************/
+ *
+ *  The original Work has been changed by NXP.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Copyright 2020-2023 NXP
+ *
+ ******************************************************************************/
 #pragma once
 #include <vector>
 #include "NfcJniUtil.h"
 #include "NfcStatsUtil.h"
 #include "SyncEvent.h"
 #include "nfa_rw_api.h"
-#if (NXP_EXTNS == TRUE)
-#define MIN_FWI (0)
+
+#define MIN_FWI (11)
 #define MAX_FWI (14)
+#if (NXP_EXTNS == TRUE)
 #define NON_STD_CARD_SAK (0x13)
 #define TIME_MUL_100MS 100
 
