e8f77750e674344c2d9653a44ee429235557b945
sai.shwethas <sai.shwethas@nxp.com>
2023-11-29 14:39:01 +0530

    nfc: Use NfcServiceManager register method for adding NFC service
    Bug: 269179724
    Test: atest CtsNfcTestCases
    Change-Id: Iedbcad4d189dce4c4a10be4c6783f1ac48008612

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -59,6 +59,8 @@ import android.media.AudioAttributes;
 import android.media.SoundPool;
 import android.net.Uri;
 import android.nfc.AvailableNfcAntenna;
+import android.nfc.NfcFrameworkInitializer;
+import android.nfc.NfcServiceManager;
 import android.nfc.cardemulation.CardEmulation;
 import android.nfc.ErrorCodes;
 import android.nfc.FormatException;
@@ -951,7 +953,12 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         mPollingDisableAllowed = mContext.getResources().getBoolean(R.bool.polling_disable_allowed);
 
         // Make sure this is only called when object construction is complete.
-        ServiceManager.addService(SERVICE_NAME, mNfcAdapter);
+        NfcServiceManager manager = NfcFrameworkInitializer.getNfcServiceManager();
+        if (manager == null) {
+            Log.e(TAG, "NfcServiceManager is null");
+            throw new UnsupportedOperationException();
+        }
+        manager.getNfcManagerServiceRegisterer().register(mNfcAdapter);
 
         mIsAlwaysOnSupported =
             mContext.getResources().getBoolean(R.bool.nfcc_always_on_allowed);
