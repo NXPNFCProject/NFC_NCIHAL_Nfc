55efd867f4e0dde951bac8e2601139de3560b7d7
nxf31698 <suryaprakash.konduru@nxp.com>
2024-07-12 15:59:23 +0530

    Fix NATIVE_CRASH in com.android.nfc: null pointer dereference
    Check if NFC is enabled before doing tag operation
    
    Bug: 333165026
    Test: manual
    
    Change-Id: Id5de6a1a9a9627df1e9d0c0a8da0ef347155356b

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -4685,6 +4685,8 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                 }
 
                 case MSG_NDEF_TAG:
+                    if (!isNfcEnabled())
+                        break;
                     if (DBG) Log.d(TAG, "Tag detected, notifying applications");
 
                     clearAppInactivityDetectionContext();
