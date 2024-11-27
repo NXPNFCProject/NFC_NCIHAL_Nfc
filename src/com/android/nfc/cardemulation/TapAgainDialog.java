c881ebd45be747d4d7e55c511715f47070a8e900
nxf45198 <fahim.akhtar@nxp.com>
2024-11-27 14:45:07 +0530

    TextView is not used in this class. It is recommended to delete it. Invalid import is introduced.
    bug: Just a small optimization, no need to use bug tracking.
    link: na.
    
    URL : https://android-review.googlesource.com/c/platform/packages/apps/Nfc/+/3247831
    
    Change-Id: Ida21816f023fc4af71ce8bc4494660ad027507eb

--- src/com/android/nfc/cardemulation/TapAgainDialog.java
+++ src/com/android/nfc/cardemulation/TapAgainDialog.java
@@ -32,7 +32,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
 
 import com.android.nfc.cardemulation.util.AlertActivity;
