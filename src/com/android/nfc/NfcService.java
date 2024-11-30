0cc748fd2899f2d3dee4650752c5a773b345959e
nxf45198 <fahim.akhtar@nxp.com>
2024-11-30 22:11:47 +0530

    According to the logic here, '||' may be more appropriate than '|'
    '|' regardless of whether the previous one is true, calculate the next one, '||' maybe better.
    
    bug: 362618449
    link: https://partnerissuetracker.corp.google.com/u/0/issues/362618449
    
    URL : https://android-review.googlesource.com/c/platform/packages/apps/Nfc/+/3243926
    
    Change-Id: I8185ed70641573e47cbd8c580e16f74b32af125a

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -2739,7 +2739,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
             }
             List<AvailableNfcAntenna> availableNfcAntennas = new ArrayList<>();
             for(int i = 0; i < positionX.length; i++){
-                if(positionX[i] >= width | positionY[i] >= height){
+                if(positionX[i] >= width || positionY[i] >= height){
                     return null;
                 }
                 availableNfcAntennas.add(new AvailableNfcAntenna(positionX[i], positionY[i]));
