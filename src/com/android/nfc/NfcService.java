02a4ef95e828c3e2ad53ab149c8478c0c19921ff
Balaramasamy M C <balaramasamymc.x@nxp.com>
2024-12-02 12:43:54 +0100

    Revert "[artf1198197][AOSP][NfcApp] Include package name in log when change"
    This reverts commit dffcd1d89b7cfd549679381c40f709b490bf37b9.
    
    Reason for revert: <Compilation failed>
    
    Change-Id: Iacb07bad1aed75829dde5cea418f78055ad34273

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -136,7 +136,6 @@ import com.android.nfc.cardemulation.RegisteredAidCache;
 import com.android.nfc.cardemulation.RoutingOptionManager;
 import com.android.nfc.DeviceHost.DeviceHostListener;
 import com.android.nfc.DeviceHost.TagEndpoint;
-import com.android.nfc.NfcService.EnableDisableTask;
 import com.android.nfc.dhimpl.NativeNfcManager;
 import com.android.nfc.dhimpl.NativeNfcSecureElement;
 import com.android.nfc.flags.FeatureFlags;
@@ -3011,7 +3010,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         }
 
         @Override
-        public boolean enableReaderOption(boolean enable, String pkg) {
+        public boolean enableReaderOption(boolean enable) {
             Log.d(TAG, "enableReaderOption enabled=" + enable);
             if (!mReaderOptionCapable) return false;
             NfcPermissions.enforceAdminPermissions(mContext);
@@ -3029,11 +3028,6 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                                         NfcEventProto.NfcReaderOptionChange.newBuilder()
                                                 .setEnable(enable)
                                                 .build())
-                                                .setAppInfo(
-                                                        NfcEventProto.NfcAppInfo.newBuilder()
-                                                    .setPackageName(pkg)
-                                                    .setUid(Binder.getCallingUid())
-                                                    .build())
                                 .build());
             }
             return true;
