95e2d0565f06e651357c5bc3ff2b3da9d8149406
nxf56319 <sai.shwethas@nxp.com>
2024-12-02 10:48:19 +0530

    nfc(app): Skip routing changes when polling is disabled
    The routing table is applied when polling is resumed.
    
    Bug: 340401549
    Test: TH
    https://android-review.googlesource.com/c/platform/packages/apps/Nfc/+/3223452?forceReload=true
    URL:

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -4728,7 +4728,6 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
             if (mNfcStateCheck && !isNfcEnabledOrShuttingDown()) {
                 return;
             }
-            WatchDogThread watchDog = new WatchDogThread("applyRouting", ROUTING_WATCHDOG_MS);
             if (mInProvisionMode) {
                 mInProvisionMode = Settings.Global.getInt(mContentResolver,
                         Settings.Global.DEVICE_PROVISIONED, 0) == 0;
@@ -4737,6 +4736,10 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                     // and allow handover transfers.
                     mNfcDispatcher.disableProvisioningMode();
                 }
+            }
+            if (mPollingPaused) {
+                Log.d(TAG, "Not updating discovery parameters, polling paused.");
+                return;
             }
             // Special case: if we're transitioning to unlocked state while
             // still talking to a tag, postpone re-configuration.
@@ -4747,6 +4750,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                 return;
             }
 
+            WatchDogThread watchDog = new WatchDogThread("applyRouting", ROUTING_WATCHDOG_MS);
             try {
                 watchDog.start();
                 // Compute new polling parameters
