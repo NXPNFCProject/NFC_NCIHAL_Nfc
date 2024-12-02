efdc1561a914c4eab625afc40d88ed1a80065a99
nxf56319 <sai.shwethas@nxp.com>
2024-12-02 22:59:45 +0530

    Add security log when nfc is enable/disabled.
    Test: compile
    Bug: 319934052
    Flag: android.nfc.nfc_state_change_security_log_event_enabled
    
    URL:https://android-review.googlesource.com/c/platform/packages/apps/Nfc/+/3200690?forceReload=true

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -49,6 +49,7 @@ import android.app.KeyguardManager;
 import android.app.KeyguardManager.KeyguardLockedStateListener;
 import android.app.PendingIntent;
 import android.app.VrManager;
+import android.app.admin.SecurityLog;
 import android.app.backup.BackupManager;
 import android.app.role.RoleManager;
 import android.content.BroadcastReceiver;
@@ -2335,6 +2336,9 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                                     .setEnabled(true)
                                     .build())
                             .build());
+            if (android.nfc.Flags.nfcStateChangeSecurityLogEventEnabled()) {
+                SecurityLog.writeEvent(SecurityLog.TAG_NFC_ENABLED);
+            }
             enableNfc();
             return true;
         }
@@ -2375,6 +2379,9 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
                                     .setEnabled(false)
                                     .build())
                             .build());
+            if (android.nfc.Flags.nfcStateChangeSecurityLogEventEnabled()) {
+                SecurityLog.writeEvent(SecurityLog.TAG_NFC_DISABLED);
+            }
             new EnableDisableTask().execute(TASK_DISABLE);
 
             return true;
