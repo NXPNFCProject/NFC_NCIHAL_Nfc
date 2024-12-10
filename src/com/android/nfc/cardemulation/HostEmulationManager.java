3287432482e9045bfeaa213615b02a2e0a556c5c
suryaprakash.konduru <suryaprakash.konduru@nxp.com>
2024-12-10 13:11:02 +0530

    Fix to restore Observe mode after transaction
    - Fix to restore Observe mode in case of back to back transactions
    event is received immediately after deactivate notification.
    
    Change-Id: Iaf6cbf79ac32649c339c17b7c5e9cacd7bcd9988

--- src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -14,6 +14,26 @@
  * limitations under the License.
  */
 
+/******************************************************************************
+ *
+ *  The original Work has been changed by NXP.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Copyright 2024 NXP
+ *
+ ******************************************************************************/
+
 package com.android.nfc.cardemulation;
 
 import android.annotation.FlaggedApi;
@@ -248,17 +268,19 @@ public class HostEmulationManager {
     Runnable mEnableObserveModeAfterTransactionRunnable = new Runnable() {
         @Override
         public void run() {
-            synchronized (mLock) {
-              Log.d(TAG, "re-enabling observe mode after transaction.");
-              mEnableObserveModeAfterTransaction = false;
-              mEnableObserveModeOnFieldOff = false;
-            }
             NfcAdapter adapter = NfcAdapter.getDefaultAdapter(mContext);
             if (adapter == null) {
                 Log.e(TAG, "adapter is null, returning");
                 return;
             }
-            adapter.setObserveModeEnabled(true);
+            Log.d(TAG, "re-enabling observe mode after transaction.");
+            boolean status = adapter.setObserveModeEnabled(true);
+            if (status) {
+                synchronized (mLock) {
+                    mEnableObserveModeAfterTransaction = false;
+                    mEnableObserveModeOnFieldOff = false;
+                  }
+            }
         }
     };
 
